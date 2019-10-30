1194cf4bc4e7cc835b573067970d04fbfb0aff78
Naveen Sai Krishna <naveensaikrishna27@gmail.com>
2019-10-30 16:27:19 +0530

    Added JS Folder

---
+++ js/app.js
@@ -0,0 +1,16 @@
+var vm = new Vue({  //options
+  el:"#vue-app", // element on the html page this instance will control..
+  data: {
+    title: 'Vue Project',
+    first_name: "Naveen",
+    last_name: "Sai Krishna"
+  },
+  methods:{
+    greet: function(time){
+       return "Good "+time+', '+this.first_name+" "+this.last_name;
+    },
+    link: function(){
+      return "<a href='https://www.google.com'> "+this.title+" </a>";
+    }
+  }
+})
